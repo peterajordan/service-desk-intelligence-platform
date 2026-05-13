5435bda47fafd15dc40a4b1080298c15dd983e50
Peter Jordan <pjgoldnight@gmail.com>
2026-05-13 11:58:31 -0400

    Create escalation_engine.js

---
+++ automation/escalation_engine.js
@@ -0,0 +1,16 @@
+[
+  {
+    "ticket_id": 1001,
+    "user": "jdoe",
+    "issue": "VPN connection failure",
+    "impact": "High",
+    "urgency": "High"
+  },
+  {
+    "ticket_id": 1002,
+    "user": "asmith",
+    "issue": "Outlook not syncing",
+    "impact": "Medium",
+    "urgency": "Medium"
+  }
+]
